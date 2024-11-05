621625fa5032b39f97bcfc837e9c7603397146e8
saichandcvn <saichandcvn@gmail.com>
2024-11-06 00:10:15 +0530

    DF PySpark and SparkSQL Basic Programs

---
+++ DFSQL_002.py
@@ -0,0 +1,31 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+os.environ["PYSPARK_PYTHON"] = "C:/Users/cvnsa/Documents/Python/Python37/python.exe"
+spark = SparkSession.builder.appName("PySparkProgram").master("local[*]").getOrCreate()
+
+employees = [
+("karthik", "2024-11-01"),
+("neha", "2024-10-20"),
+("priya", "2024-10-28"),
+("mohan", "2024-11-02"),
+("ajay", "2024-09-15"),
+("vijay", "2024-10-30"),
+("veer", "2024-10-25"),
+("aatish", "2024-10-10"),
+("animesh", "2024-10-15"),
+("nishad", "2024-11-01"),
+("varun", "2024-10-05"),
+("aadil", "2024-09-30")
+]
+employees_df = spark.createDataFrame(employees, ["name", "last_checkin"])
+
+employees_df.createOrReplaceTempView("employees")
+spark.sql("""
+select initcap(name) as name, 
+datediff(current_date(), last_checkin) as datadiff,
+(case when datediff(current_date(), last_checkin) < 7 then "Active" else "Inactive" end) as workstatus
+from employees
+""").show()
+
---
+++ DFSet01_001.py
@@ -0,0 +1,17 @@
+import os
+from pyspark.sql import SparkSession
+
+os.environ["PYSPARK_PYTHON"] = "C:/Users/cvnsa/Documents/Python/Python37/python.exe"
+spark = SparkSession.builder.appName("PySparkProgram").master("local[*]").getOrCreate()
+
+schema="id int,Name string,Age int"
+
+df = spark.read \
+    .format("csv") \
+    .option("header", True) \
+    .schema(schema) \
+    .option("path", "C:/Users/cvnsa/Downloads/Datasets/info.csv") \
+    .load()
+
+df.show()
+df.printSchema()
